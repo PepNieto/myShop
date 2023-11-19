7dca676917c394d6cca0a92b7dac8876afda8f79
Pep Nieto <pepnieto97@gmail.com>
2023-11-19 23:22:11 +0100

    middleware Auth created

---
+++ src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface TokenData extends Request {
+    user?: Object; 
+  }
+
+const verificarToken = (req: TokenData, res: Response, next: NextFunction) => {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(403).json({ message: 'No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+        req.user = decoded; 
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized!' });
+    }
+};
